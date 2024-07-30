f0fb9ddb4dc5a7db6550be43d9de72224b04a833
erenyurtcu <ierenyurtcu@gmail.com>
2024-07-30 17:57:32 +0300

    interface#1
    user and feed interface created

---
+++ app/src/main/java/com/erenyurtcu/photographsharing/UserFragment.kt
@@ -0,0 +1,46 @@
+package com.erenyurtcu.photographsharing
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.erenyurtcu.photographsharing.databinding.FragmentUserBinding
+
+class UserFragment : Fragment() {
+
+    private var _binding: FragmentUserBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginButton.setOnClickListener { logIn(it) }
+        binding.signupButton.setOnClickListener { signUp(it) }
+    }
+
+    fun logIn(view: View){
+
+    }
+
+    fun signUp(view: View){
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
